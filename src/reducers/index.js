9e941042a6ce571ab3814f1b15147a99a70f45d6
designus <info@designus.lt>
2019-08-11 14:17:49 +0200

    first commit

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux';
+
+const exampleReducer = (state = null, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default combineReducers({
+  exampleReducer
+})
+  
