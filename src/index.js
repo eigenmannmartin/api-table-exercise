6b4f7e6509f8dff64bd74c36ea288480e3492cb5
Martin Eigenmann <github@eigenmannmartin.ch>
2019-08-13 19:32:09 +0200

    test(App.js) uses store in tests

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk'
-import { apiMiddleware } from 'redux-api-middleware';
-import rootReducer from './reducers';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import store from './store';
 import 'semantic-ui-css/semantic.min.css'
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, apiMiddleware)))
 
 ReactDOM.render(
   <Provider store={store}>
